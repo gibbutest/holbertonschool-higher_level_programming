f786ee30145abbccc54706429b320a3683cbe144
Ian <Gibbu_@hotmail.com>
2025-05-06 14:13:05 +1000

    Forgot "and is"

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if last > 5:
 elif last == 0:
   print(f"Last digit of {number} is {last} and is 0")
 elif last < 6 and last != 0:
-  print(f"Last digit of {number} is {last} less than 6 and not 0")
+  print(f"Last digit of {number} is {last} and is less than 6 and not 0")
