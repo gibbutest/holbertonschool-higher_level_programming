682876eadaa4a1ad9b6edcb364a0a7409ffe4ab9
Ian <Gibbu_@hotmail.com>
2025-05-12 11:09:40 +1000

    Task 6 - empty matrix?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,3 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     for a in matrix:
         for index, b in enumerate(a):
             print("{:d}".format(b), end="\n" if index == len(a) - 1 else " ")
+        print("".format())
