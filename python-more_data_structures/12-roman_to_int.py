4973afa0c57263c5521d5cf6a6233d59ef1e9140
Ian <Gibbu_@hotmail.com>
2025-05-12 16:53:03 +1000

    Task 12 - line too long

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,15 @@ def roman_to_int(roman_string):
     elif not roman_string:
         return None
 
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
     result = 0
     prev_value = 0
 
