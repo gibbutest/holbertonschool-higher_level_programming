aed679b2a66571d1c4ab21cedac8522914bf7a40
Ian <Gibbu_@hotmail.com>
2025-05-12 13:09:30 +1000

    Task 9 - init

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dic = a_dictionary.copy()
+
+    for i in new_dic.items():
+        new_dic[i[0]] = new_dic[i[0]] * 2
+    
+    return new_dic
