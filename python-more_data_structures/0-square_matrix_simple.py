bd8d1a84535a6bf6e587969d88a0ee0a2d67d1a3
Ian <Gibbu_@hotmail.com>
2025-05-12 12:12:13 +1000

    Task 0 - switched deepclone method

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-import copy
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = copy.deepcopy(matrix)
+    new_matrix = [row[:] for row in matrix]
     for aid, a in enumerate(matrix):
         for bid, b in enumerate(a):
             new_matrix[aid][bid] = new_matrix[aid][bid] ** 2
