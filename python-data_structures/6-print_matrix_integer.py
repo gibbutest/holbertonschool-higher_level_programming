d86565c58fe0360881ca02dc95ebbf1a42bcca80
Ian <Gibbu_@hotmail.com>
2025-05-12 11:10:55 +1000

    Task 6 - moved new line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for a in matrix:
         for index, b in enumerate(a):
-            print("{:d}".format(b), end="\n" if index == len(a) - 1 else " ")
+            print("{:d}".format(b), end="" if index == len(a) - 1 else " ")
         print("".format())
