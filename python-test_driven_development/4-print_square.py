7b331ad14433660f84b83b0293fc6b443f555900
Ian <Gibbu_@hotmail.com>
2025-05-18 18:11:31 +1000

    Task 4 - -_-

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,8 +9,8 @@ def print_square(size):
         A function that prints a square of #
     """
     if not isinstance(size, int):
-        raise TypeError("Size must be an integer")
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("Size must be >= 0")
+        raise ValueError("size must be >= 0")
     for i in range(size):
         print(f'#' * size)
