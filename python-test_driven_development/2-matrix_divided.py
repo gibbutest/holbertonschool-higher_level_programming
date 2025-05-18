ffca62628755325ea8718527e138d387eb5c33f1
Ian <Gibbu_@hotmail.com>
2025-05-18 17:53:33 +1000

    Task 2 - style guide????

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,22 +6,22 @@
 
 def matrix_divided(matrix, div):
     """
-        Divides all elements of a matrix by a number, rounding to 2 decimal places.
-        Then returning a new matrix.
+        Divides all elements of a matrix by a number,
+        rounding to 2 decimal places. Then returning a new matrix.
     """
     if not isinstance(div, int):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     if (not isinstance(matrix, list) or
         not all(isinstance(row, list) for row in matrix) or
-        not all(isinstance(x, (int, float)) for row in matrix for x in row)):
+            not all(isinstance(x, (int, float)) for row in matrix for x in row)):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
-    
+
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
         raise TypeError(
