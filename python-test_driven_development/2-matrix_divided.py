8c108797c2f95150a7a08b72a39e1114178ad033
Ian <Gibbu_@hotmail.com>
2025-05-18 17:50:45 +1000

    Task 2 - style guide -_-

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,10 +18,14 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(x, (int, float)) for row in matrix for x in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
     
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError(
+            "Each row of the matrix must have the same size"
+        )
 
     return [[round(val / div, 2) for val in row] for row in matrix]
