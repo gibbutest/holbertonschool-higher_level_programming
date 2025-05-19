014903e1e88d6d20d5c0edaee46137d3cb630c3a
Ian <Gibbu_@hotmail.com>
2025-05-19 12:39:50 +1000

    Task 6 - only run for loops if above 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -61,9 +61,9 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
 
-        for _ in range(self.__position[1]):
-            print()
-
-        for _ in range(self.__size):
-            print(' ' * self.__position[0] + '#' * self.__size)
+            for _ in range(self.__size):
+                print(' ' * self.__position[0] + '#' * self.__size)
