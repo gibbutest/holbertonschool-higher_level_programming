cb3dbb2344a249277395408e60a40e16e853e14d
Ian <Gibbu_@hotmail.com>
2025-05-19 11:17:35 +1000

    Task 1 - init

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    The square module
+"""
+
+
+class Square:
+    """
+        A class to create a square
+    """
+
+    def __init__(self, size):
+        self.__size = size
