adf0568c44e153d9358a6d859baeb973f29c890a
Ian <Gibbu_@hotmail.com>
2025-05-27 11:11:18 +1000

    Task 4 - oops

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ Checks if a instance of class that inherits from a specific class """
-    isinstance(obj, a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
