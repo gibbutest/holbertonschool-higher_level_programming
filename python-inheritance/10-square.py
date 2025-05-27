da90df31cc629c0f987a4000e95cba173c033ec1
Ian <Gibbu_@hotmail.com>
2025-05-27 13:18:24 +1000

    Task 9 - style guide -_-

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" The module """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        super().integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
