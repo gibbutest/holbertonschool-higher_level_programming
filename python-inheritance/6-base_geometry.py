e3a044451d35d617c411cdeec9e501f9e509883d
Ian <Gibbu_@hotmail.com>
2025-05-27 11:35:58 +1000

    Task 6 - init

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,3 +7,9 @@ class BaseGeometry:
 
     def area(self):
         raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if isinstance(value, int):
+            raise TypeError('<name> must be an integer')
+        elif value <= 0:
+            raise ValueError('<name> must be greater than 0')
