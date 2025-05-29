77a8ad0804f501e51840d65db0239667e27760ae
Ian <Gibbu_@hotmail.com>
2025-05-29 11:03:36 +1000

    Task 3 - init

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" The module """
+
+
+class CountedIterator():
+    """ Wack iteration class """
+
+    def __init__(self, data):
+        self.__counter = 0
+        self.data = data
+
+    def __next__(self):
+        if self.__counter >= len(self.data):
+            raise StopIteration()
+
+        self.__counter += 1
+        return self.data[self.__counter]
+
+    def get_count(self):
+        return self.__counter
