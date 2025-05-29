dc40771c1b7c098f4e2664fce8ad275a59949323
Ian <Gibbu_@hotmail.com>
2025-05-29 10:40:20 +1000

    Task 0 - add self to sound function

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -8,19 +8,19 @@ class Animal(ABC):
     """ Abstract animal class """
 
     @abstractmethod
-    def sound():
+    def sound(self):
         pass
 
 
 class Dog(Animal):
     """ The dog class """
 
-    def sound():
+    def sound(self):
         return "Bark"
     
 
 class Cat(Animal):
     """ The cat class """
 
-    def sound():
+    def sound(self):
         return "Meow"
