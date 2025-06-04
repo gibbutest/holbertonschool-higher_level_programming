4513d6c4d325c19fcd691d7f594ae578d626945f
Ian <Gibbu_@hotmail.com>
2025-06-04 10:14:05 +1000

    Task 7 - init

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" The module """
+
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+new_list = list()
+
+try:
+    new_list = load_from_json_file("add_item.json")
+except:
+    pass
+
+new_list.extend(argv[1:])
+
+save_to_json_file(new_list, "add_item.json")
