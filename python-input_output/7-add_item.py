e1d2324809c80b929c074167ca04b3349dcc1992
Ian <Gibbu_@hotmail.com>
2025-06-04 10:17:21 +1000

    Task 7 - wrong except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,12 +7,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-new_list = list()
-
 try:
     new_list = load_from_json_file("add_item.json")
-except FileExistsError:
-    pass
+except FileNotFoundError:
+    new_list = list()
 
 new_list.extend(argv[1:])
 
