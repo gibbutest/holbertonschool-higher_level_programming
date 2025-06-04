31deb7b490f55250b27ddfa98e1772a98c800f6f
Ian <Gibbu_@hotmail.com>
2025-06-04 10:07:44 +1000

    Task 6 - different method

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,8 +2,11 @@
 """ The module """
 
 
+import json
+
+
 def load_from_json_file(filename):
     """ Load json file """
 
     with open(filename) as file:
-        return file.read()
+        return json.load(file)
