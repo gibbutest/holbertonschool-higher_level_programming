af124e109daf0685b560024835cb60ace906ceae
Ian <Gibbu_@hotmail.com>
2025-06-09 16:52:56 +1000

    Task 3 - maybe>

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -23,18 +23,12 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(b'OK')
-        elif self.path == '/undefined':
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-
-            self.wfile.write(b"404 Not Found")
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
 
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 
