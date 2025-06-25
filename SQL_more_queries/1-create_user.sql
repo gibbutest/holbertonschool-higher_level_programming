563631f7bc775715351b8f9299d3f6f40a6defee
Ian <Gibbu_@hotmail.com>
2025-06-25 11:14:28 +1000

    Task 1 - formatting

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- I AM GOD. I CREATE A NEW USER!
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 
+  'user_0d_1'@'localhost'
+IDENTIFIED BY 
+  'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
