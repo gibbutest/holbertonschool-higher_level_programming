893d688282fff4a636dbf297e4ea3c58e02ffb45
Ian <Gibbu_@hotmail.com>
2025-06-25 11:38:08 +1000

    Task 6 - init

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- All my homies don't enjoy the USA
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE
+  IF NOT EXISTS hbtn_0d_usa.states (
+    id int AUTO_INCREMENT,
+    name varchar(256) NOT NULL,
+    PRIMARY KEY (id)
+  );
