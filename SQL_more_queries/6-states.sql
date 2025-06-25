83eb46fdc706662030dc1226a43b7c578992d449
Ian <Gibbu_@hotmail.com>
2025-06-25 11:52:30 +1000

    Task 6 - edit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,10 @@
 -- All my homies don't enjoy the USA
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+use hbtn_0d_usa;
+
 CREATE TABLE
-  IF NOT EXISTS hbtn_0d_usa.states (
+  IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
   );
