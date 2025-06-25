d20b692361b16fd5ffc6efe007697ab4e1e4715a
Ian <Gibbu_@hotmail.com>
2025-06-25 11:56:49 +1000

    Task 8 - init

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,17 @@
+-- We going sub querying bruh
+SELECT
+  id,
+  name
+FROM
+  `cities`
+WHERE
+  state_id = (
+    SELECT
+      id
+    FROM
+      states
+    WHERE
+      name = 'California'
+  )
+ORDER BY
+  id ASC
