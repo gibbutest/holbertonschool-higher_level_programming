cb703a5c8e6ee76389524bbb9fdb294b3601ca9a
Ian <Gibbu_@hotmail.com>
2025-06-25 13:01:30 +1000

    Task 13 - uppercase

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- All the genres
 SELECT
-  tv_genres.name as genre,
-  COUNT(tv_show_genres.show_id) as number_of_shows
+  tv_genres.name AS genre,
+  COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM
   tv_genres
   LEFT JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
