463d3ad7be28933572266752f506b849b2cb1765
Ian <Gibbu_@hotmail.com>
2025-06-30 09:40:45 +1000

    Task 11 - init

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" The module """
+
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+    )
+    session = sessionmaker(bind=engine)()
+
+    new_state = State(name='Louisiana')
+
+    session.add(new_state)
+    session.commit()
+
+    state = session.query(State.id).where(State.name == 'Louisiana').first()
+
+    if state:
+        print(state[0])
+    else:
+        print("Not found")
