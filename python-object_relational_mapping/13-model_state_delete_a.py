e4d13d1accbcef9fc90de9baa5ab048739beb575
Ian <Gibbu_@hotmail.com>
2025-06-30 10:06:10 +1000

    Task 13 - init

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" The module """
+
+from model_state import State
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+    )
+    session = sessionmaker(bind=engine)()
+
+    state = session.execute(delete(State).where(State.name.contains('a')))
+    session.commit()
