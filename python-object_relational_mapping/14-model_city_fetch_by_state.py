8c52089198c87f88fa9992b72c509ce62cb942c9
Ian <Gibbu_@hotmail.com>
2025-06-30 10:12:32 +1000

    Task 14 - init

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" The module """
+
+from model_state import State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}'
+    )
+    session = sessionmaker(bind=engine)()
+
+    rows = session.query(State, City).join(State, City.state_id == State.id).order_by(City.id)
+
+    for city, state in rows:
+        print(f"{state.name}: ({city.id}) {city.name}")
