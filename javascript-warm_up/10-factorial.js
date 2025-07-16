c276e9927f8d12b56122b5e6d9f3ef5a08b8782d
Ian <Gibbu_@hotmail.com>
2025-07-16 11:07:48 +1000

    Task 10 - semistandard, more like semishit

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ const num = parseInt(process.argv[2]);
 
 // prettier-ignore
 function factorial (n) {
-  return isNaN(n) || n <= 0 ? 1 : n * factorial(n - 1)
+  return isNaN(n) || n <= 0 ? 1 : n * factorial(n - 1);
 }
 
 console.log(factorial(num));
